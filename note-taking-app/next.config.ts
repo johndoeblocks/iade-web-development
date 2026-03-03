c5c023aea0aef7cc5284beae7450ba613e6b4578
JohnDoeBlocks <joaooliveiramanteigas@gmail.com>
2026-03-03 12:56:26 +0000

    Fix nextjs vercel build issues

--- note-taking-app/next.config.ts
+++ note-taking-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/notes": ["./data/**/*"],
+    "/api/notes/[id]": ["./data/**/*"],
+  },
 };
 
 export default nextConfig;
